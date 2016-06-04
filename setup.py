e13b9164d3e322706ed0b853b39e5434a4c6d305
ra100 <git@ra100.net>
2016-06-04 14:40:49 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='cura2slic3r',
     version='0.1',
     description='Converts Cura slicer profile do Slic3r profile',
-    author='ra100',
+    author='Rastislav Svarba (ra100)',
     author_email='git@ra100.net',
     url='https://github.com/ra100/curaToSlic3rProfile',
+    license='GPLv3+',
     packages = ['package'],
-    scripts=['cura2slic3r']
+    scripts=['cura2slic3r'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Utilities',
+        'Environment :: Console',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
